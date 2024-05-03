4e1354dc166aeb746581270c0b61ecd8c3354ade
tuansgu <nguyentuan01082003t@gmail.com>
2024-05-03 17:41:58 +0700

    Update: adminlayout

--- app/views/blocks/admin/navAdmin.php
+++ app/views/blocks/admin/navAdmin.php
@@ -1,103 +1,4 @@
 <aside class="left-sidebar">
-<<<<<<< HEAD
-      <!-- Sidebar scroll-->
-      <div>
-        <div class="brand-logo d-flex align-items-center justify-content-between">
-          <a href="./index.html" class="text-nowrap logo-img">
-            <img src="../assets/images/logos/dark-logo.svg" width="180" alt="" />
-          </a>
-          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-            <i class="ti ti-x fs-8"></i>
-          </div>
-        </div>
-        <!-- Sidebar navigation-->
-        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-          <ul id="sidebarnav">
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">Trang chủ</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./index.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-layout-dashboard"></i>
-                </span>
-                <span class="hide-menu">Dashboard</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-login"></i>
-                </span>
-                <span class="hide-menu">Quản lý tài khoản</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-login"></i>
-                </span>
-                <span class="hide-menu">Quản lý nhân viên</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-login"></i>
-                </span>
-                <span class="hide-menu">Quản lý sản phẩm</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-login"></i>
-                </span>
-                <span class="hide-menu">Quản lý đơn hàng</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="<?php echo _WEB_ROOT; ?>/ImportController" aria-expanded="false">
-                <span>
-                  <i class="ti ti-login"></i>
-                </span>
-                <span class="hide-menu">Quản lý nhập hàng</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-login"></i>
-                </span>
-                <span class="hide-menu">Quản lý bảo hành</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-login"></i>
-                </span>
-                <span class="hide-menu">Thống kê</span>
-              </a>
-            </li>
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">Chức năng</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-login"></i>
-                </span>
-                <span class="hide-menu">Đăng xuất</span>
-              </a>
-            </li>
-          </ul>
-
-        </nav>
-        <!-- End Sidebar navigation -->
-=======
   <!-- Sidebar scroll-->
   <div>
     <div class="brand-logo d-flex align-items-center justify-content-between">
@@ -106,7 +7,6 @@
       </a>
       <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
         <i class="ti ti-x fs-8"></i>
->>>>>>> 7ff08fd300ac7acd50b046988fe67ef4ad4fada9
       </div>
     </div>
     <!-- Sidebar navigation-->
@@ -141,7 +41,7 @@
           </a>
         </li>
         <li class="sidebar-item">
-        <a class="sidebar-link" href="<?php echo _WEB_ROOT; ?>/admin/product" aria-expanded="false">
+          <a class="sidebar-link" href="<?php echo _WEB_ROOT; ?>/admin/product" aria-expanded="false">
             <span>
               <i class="ti ti-login"></i>
             </span>
@@ -157,7 +57,7 @@
             <span class="hide-menu">Quản lý đơn hàng</span>
           </a>
         </li>
-      
+
         <li class="sidebar-item">
           <a class="sidebar-link" href="<?php echo _WEB_ROOT; ?>/admin/insurance" aria-expanded="false">
             <span>
@@ -167,7 +67,7 @@
           </a>
         </li>
         <li class="sidebar-item">
-        <a class="sidebar-link" href="<?php echo _WEB_ROOT; ?>/thongke" aria-expanded="false">
+          <a class="sidebar-link" href="<?php echo _WEB_ROOT; ?>/thongke" aria-expanded="false">
             <span>
               <i class="ti ti-login"></i>
             </span>
