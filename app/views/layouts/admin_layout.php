a5d76cccad03493c43ffdb8b12eb0806f8f4ee78
tuansgu <nguyentuan01082003t@gmail.com>
2024-05-10 15:41:09 +0700

    Dell co gi

--- app/views/layouts/admin_layout.php
+++ app/views/layouts/admin_layout.php
@@ -49,10 +49,10 @@
     <script src="<?php echo _WEB_ROOT; ?>/public/assets/admin/js/app.min.js"></script>
     <script src="<?php echo _WEB_ROOT; ?>/public/assets/admin/libs/apexcharts/dist/apexcharts.min.js"></script>
     <script src="<?php echo _WEB_ROOT; ?>/public/assets/admin/libs/simplebar/dist/simplebar.js"></script>
-    <script src="<?php echo _WEB_ROOT; ?>/public/assets/admin/js/productAdmin.js"></script>
+    <script src="<?php echo _WEB_ROOT; ?>/public/assets/admin/js/productAddmin.js"></script>
+    <script src="<?php echo _WEB_ROOT; ?>/public/assets/admin/js/insuranceAdddmin.js"></script>
     <script src="<?php echo _WEB_ROOT; ?>/public/assets/admin/js/dashboard.js"></script>
     <script src="<?php echo _WEB_ROOT; ?>/node_modules/toastr/toastr.js"></script>
-    <script src="<?php echo _WEB_ROOT; ?>/public/assets/admin/js/dashboard.js"></script>
 
 </body>
 
